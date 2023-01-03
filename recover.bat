bda2ecdd0ac99732590304682d60f832880017a1
TwInStartLine <120017154+TwInStartLine@users.noreply.github.com>
2023-01-03 10:42:16 +0900

    Do not remove study folder.

--- recover.bat
+++ recover.bat
@@ -3,5 +3,5 @@
 subst s: /d
 
 set EDU_PATH="c:\CodingChildren"
-rmdir %EDU_PATH% /s
+rem rmdir %EDU_PATH% /s
 Reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v AutoGo /f 
